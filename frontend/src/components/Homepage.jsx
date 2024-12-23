a88cd10937b321af88d47ee7c50c93bc745076f3
Rahul <venkatrahulxyz@gmail.com>
2024-12-23 14:52:33 +0530

    completed hero part

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,31 +1,31 @@
-import classes from "./Homepage.module.css"
+import classes from "./Homepage.module.css";
 
-function Homepage(){
+function Homepage() {
     return (
-    <div className={classes.homepage}>
-      {/* Navbar */}
-      <nav className={classes.navbar}>
-        <div className={classes.logo}><img src="./src/assets/Logo.png" alt="" /></div>
-        <div className={classes.navLinks}>
-          <a href="#products">Home</a>
-          <a href="#pricing">About</a>
-          <a href="#resources">Contact</a>
-        </div>
-      </nav>
+        <div className={classes.homepage}>
+            {/* Navbar */}
+            <nav className={classes.navbar}>
+                <div className={classes.logo}><img src="./src/assets/Logo.png" alt="Logo" /></div>
+                <div className={classes.navLinks}>
+                    <a href="#products">Home</a>
+                    <a href="#pricing">About</a>
+                    <a href="#resources">Contact</a>
+                </div>
+            </nav>
 
-      {/* Hero Section */}
-      <section className={classes.hero}>
-        <div className={classes.heroText}>
-          <h1>Send unlimited Emails in Seconds with InstaMail</h1>
-          <p>Easily send bulk emails that matter – tailored and on time.</p>
-          <button className={classes.signUpButton}>Get Started for Free</button>
+            {/* Hero Section */}
+            <section className={classes.hero}>
+                <div className={classes.heroText}>
+                    <h1>Send Unlimited Emails in Seconds with InstaMail</h1>
+                    <p>Easily send bulk emails that matter – tailored and on time.</p>
+                    <button className={classes.signUpButton}><strong>Get Started for Free</strong></button>
+                </div>
+                <div className={classes.heroImage}>
+                    <img src="./src/assets/hero-img-removebg.png" alt="Email Campaign Screenshot" />
+                </div>
+            </section>
         </div>
-        <div className={classes.heroImage}>
-          <img src="/path/to/your-image.png" alt="Email Campaign Screenshot" />
-        </div>
-      </section>
-    </div>
-    )
+    );
 }
 
 export default Homepage;
